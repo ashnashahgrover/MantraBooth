1b968696033eb9d0915f86eda4ba3d2fa3a028e6
Ashna Shah-Grover <as19@williams.edu>
2020-01-15 11:23:47 -0500

    Made a temporary set of wishes to simulate wish pool - playing with how to render onto screen

--- my-project-client/src/App.js
+++ my-project-client/src/App.js
@@ -1,43 +1,61 @@
-import React from 'react';
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
 import movie from './movies/earth.mov';
 
 
-function App() {
-  return (
-    <div>
-      {/*<header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        {/*<video
-        style={{
-          position: "fixed",
-          width: "100%",
-          left: 0,
-          top: 0
-        }}
-        autoPlay loop muted>
-        <source src={movie}  />
-        </video>*/}
-        <div class="el-wrap x">
-        <div class="el y">HELLO</div>
-        </div>
-{/*
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>*/}
-      {/*</header>*/}
-    </div>
+class App extends Component {
 
-  );
+  constructor(props){
+    super(props)
+    let messages=[]
+    for (let i=0; i<100;i++){
+      messages.push(`Hello ${i}`)
+    }
+    this.state={messages:messages}
+  }
+
+  generateFlyingWishes = () => {
+    //generate three-five(?) random numbers and make a div/li for each of them which will fly around the screen
+    //upon hover, becomes animated(invites user to click)
+    //upon clicking .... () 
+  }
+
+  render(){
+    return (
+      <div>
+        {/*<header className="App-header">
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          {/*<video
+          style={{
+            position: "fixed",
+            width: "100%",
+            left: 0,
+            top: 0
+          }}
+          autoPlay loop muted>
+          <source src={movie}  />
+          </video>*/}
+          <div className="el-wrap x">
+          <div className="el y">HELLO</div>
+          </div>
+  {/*
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>*/}
+        {/*</header>*/}
+      </div>
+
+    )
+  }
 }
 
 export default App;
